396b2367cbd29abf232138aecb2bf9ae5365366a
mohitgenii <mohitgenii@7564f8d8-1b4d-0410-a477-2548c2af9818>
2008-07-10 06:46:08 +0000

    Resolution independent code, with animation added and one can move around the village, and also zoom in/out Yet to add villagers sprite to it

--- src/Exceptions.py
+++ src/Exceptions.py
@@ -1,34 +1,1 @@
-#! /usr/bin/env python
-#
-#   Author : Mohit Taneja (mohitgenii@gmail.com)
-#   Date : 02/06/2008 
-#
-#   This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 2 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program; if not, write to the Free Software
-#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-#
-
-class Low_Manpower_Resources_Exception(Exception):
-    pass
-
-class Resources_Underflow_Exception(Exception):
-    pass
-
-class Money_Underflow_Exception(Exception):
-    pass
-
-class Resources_Overflow_Exception(Exception):
-    pass
-
-class Maximum_Level_Reached(Exception):
-    pass
+#! /usr/bin/env python##   Author : Mohit Taneja (mohitgenii@gmail.com)#   Date : 02/06/2008 ##   This program is free software; you can redistribute it and/or modify#   it under the terms of the GNU General Public License as published by#   the Free Software Foundation; either version 2 of the License, or#   (at your option) any later version.##   This program is distributed in the hope that it will be useful,#   but WITHOUT ANY WARRANTY; without even the implied warranty of#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the#   GNU General Public License for more details.##   You should have received a copy of the GNU General Public License#   along with this program; if not, write to the Free Software#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.#class Low_Manpower_Resources_Exception(Exception):    passclass Resources_Underflow_Exception(Exception):    passclass Money_Underflow_Exception(Exception):    passclass Resources_Overflow_Exception(Exception):    passclass Maximum_Level_Reached(Exception):    pass    class Maximum_Number_Reached(Exception):    pass
