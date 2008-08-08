68afa62ae65e11f156b96bbcd8f229a2082fc003
mohitgenii <mohitgenii@7564f8d8-1b4d-0410-a477-2548c2af9818>
2008-08-08 17:39:03 +0000

    Positions of all the facilities have been fixed. Ther village has been divided into clusters and development takes place in levels. The villagers are generated with each facility seperately. Images of each facility are shown in the setup window when selected for installation.

--- src/olpcgames/__init__.py
+++
@@ -1,7 +0,0 @@
-from canvas import *
-from activity import *
-import camera
-import pangofont
-import mesh
-
-widget = None
--- src/setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup
-import py2exe
-
-setup(windows = [{"script":"display_panel.py"}],
-      data_files=[('data', ["data\earthquake1.png","data\earthquake2.png","data\earthquake3.png",'data\Farm.png','data\Fountain.png','data\hospital_upgrade0.png','data\hospital_upgrade1.png','data\hospital_upgrade2.png','data\hospital_upgrade3.png','data\house_upgrade0.png','data\house_upgrade1.png','data\house_upgrade2.png','data\house_upgrade3.png','data\school_upgrade0.png','data\school_upgrade1.png','data\school_upgrade2.png','data\school_upgrade3.png','data\workshop_upgrade0.png','data\workshop_upgrade1.png','data\workshop_upgrade2.png','data\workshop_upgrade3.png','data\market.png','data\people.png','data\Tileset.png','data\Top.png','data\soundtrack.ogg']),('art', ['art\Button.png', 'art\closebutton.png', 'art\optionbox.png','art\shadebutton.png','art\checkbox.png','art\combobox.png']),('.',['font.ttf']) ])
