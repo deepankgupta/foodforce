90574c14222378012dca54116226720924a82dc2
mohitgenii <mohitgenii@7564f8d8-1b4d-0410-a477-2548c2af9818>
2009-11-08 19:19:51 +0000

    Deleted the src_xo folder and extra images from data folder

--- src_xo/activity.py
+++
@@ -1,9 +0,0 @@
-from olpcgames import activity
-from gettext import gettext as _
-
-class Activity(activity.PyGameActivity):
-    """Your Sugar activity"""
-    
-    game_name = 'Foodforce2:main'
-    game_title = _('FoodForce2')
-    game_size = None
--- src_xo/profiling.py
+++
@@ -1,8 +0,0 @@
-import profile
-import pstats
-
-profile.run('import Foodforce2; Foodforce2.main()', 'profile.tmp')
-       
-p = pstats.Stats('profile.tmp')
-p.sort_stats('cumulative').print_stats(1000)
-       
